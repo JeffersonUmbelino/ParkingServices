50069c0d3a56a5a95a0412b6cd9daaec8f0d2432
sergio costa <prof.sergio.costa@gmail.com>
2019-06-04 07:36:26 -0300

    atualizado o parking controller

--- app/Controllers/Http/ParkingController.js
+++ app/Controllers/Http/ParkingController.js
@@ -4,77 +4,102 @@ const Helpers = use('Helpers')
 const Parking = use('App/Models/Parking')
 const Geo = require('geo-nearby');
 
+const Database = use('Database')
+
 class ParkingController {
 
   //listas or registros
-  async index ({request}) {
+  async index({ request }) {
 
-  
-
-    const {latitude, longitude} = request.all()
+    const { latitude, longitude } = request.all()
     const dist = 5000;
     const parkings = await Parking
       .query()
       .with('images')
       .fetch()
 
-    const dataset = new Geo(parkings.rows, {setOptions: {id: 'id', lat: 'latitude', lon: 'longitude'}});
-    const nears = dataset.nearBy(latitude, longitude, dist);
-    const nearside = nears.map(g => g.i);
-    parkings.rows = parkings.rows.filter( (r) => nearside.includes(r.id) );
-    return parkings;
+    if (latitude && longitude) {
+      const dataset = new Geo(parkings.rows, { setOptions: { id: 'id', lat: 'latitude', lon: 'longitude' } });
+      const nears = dataset.nearBy(latitude, longitude, dist);
+      const nearside = nears.map(g => g.i);
+      parkings.rows = parkings.rows.filter((r) => nearside.includes(r.id));
+    }
+
+    // descobrir a quantidade de vagas
+    const anAsyncFunction = async r => {
+      let occupation = await Database
+        .table('parkings')
+        .innerJoin('positions', 'parkings.id', 'positions.parking_id')
+        .where({ "positions.occupation": "true", "parkings.id": r.id })
+        .count();
+      return { "vacancies": r.total_vacancies - occupation[0]["count(*)"], "id": r.id } // adicionar os atributos que precisamos
+    }
+    const getData = async () => {
+      return await Promise.all(parkings.rows.map(item => anAsyncFunction(item)))
+    }
+    const rows = await getData()
+    return rows;
+
+    // return parkings
 
   }
 
- //exibição de registros
-  async show ({params}) {
+  //exibição de registros
+  async show({ params }) {
 
-      const parkings = await Parking.findOrFail(params.id)
+    let occupation = await Database
+      .table('parkings')
+      .innerJoin('positions', 'parkings.id', 'positions.parking_id')
+      .where({ "positions.occupation": "true", "parkings.id": params.id })
+      .count();
+    // ainda falta adicionar a saida, talvez vamos ter que fazer o mesmo que no outro metodo, recriar um json
+    
+    const parkings = await Parking.findOrFail(params.id)
 
-      await parkings.load('images')
+    await parkings.load('images')
 
-      return parkings
+    return parkings
 
   }
 
   //Cria novos registros
-  async store ({ auth, request, response }) {
+  async store({ auth, request, response }) {
 
-      const {id} = auth.user
-      const data = request.only([
+    const { id } = auth.user
+    const data = request.only([
 
-          'title',
-          'address',
-          'latitude',
-          'longitude',
-          'price',
-          'total_vacancies'
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price',
+      'total_vacancies'
 
-        ])
+    ])
 
-      const parking = await Parking.create({...data, user_id: id })
+    const parking = await Parking.create({ ...data, user_id: id })
 
-      return parking
+    return parking
 
   }
 
 
   //altera registros
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
 
     const parking = await Parking.findOrFail(params.id)
 
     const data = request.only([
 
-          'title',
-          'address',
-          'latitude',
-          'longitude',
-          'price',
-          'total_vacancies'
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price',
+      'total_vacancies'
 
 
-      ])
+    ])
 
     parking.merge(data)
 
@@ -84,15 +109,15 @@ class ParkingController {
 
   }
 
- //Remove registros
-  async destroy ({ params, auth, response }) {
+  //Remove registros
+  async destroy({ params, auth, response }) {
     const parkings = await Parking.findOrFail(params.id)
 
-    if(parkings.user_id != auth.user.id){
-      return response.status(401).send({error: 'Not authorized'})
+    if (parkings.user_id != auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
     }
 
-      await parkings.delete()
+    await parkings.delete()
 
   }
 }
